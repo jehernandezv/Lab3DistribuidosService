42f2414eb9cf49fcc7fec8bcbdcad0d1a6e74234
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-04-14 21:46:33 -0500

    se configuro  el servidor con middleware para la gestin de imagenes, tambien la forma en como se manejaran los json enviados por el frontend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const multer = require('multer');
+ const path = require('path');
 
 //inicializaciones
 const app = express();
 
  //settings
  app.set('port',3000);
+
+ //middleware
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,'public/uploads'),
+    filename(req,file,cb){
+        cb(null,new Date().getTime() + path.extname(file.originalname));
+    }   
+})
+app.use(multer(storage).single('image'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 //iniciar servidor
 app.listen(app.get('port'),()=>{
